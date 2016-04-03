60124f0e3fe8c8c152f639cf6020893edc64d19e
bornytm <bornytm@gmail.com>
2016-04-03 00:54:13 -0700

    don't need iscontrol file...

--- meet_the_participants/isocontrol.js
+++ meet_the_participants/isocontrol.js
@@ -1,39 +1,39 @@
-// var elem = document.querySelector('.grid');
-var _iso = new Isotope( '.grid', {
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows',
-  gutter: '20px'
-});
+// // var elem = document.querySelector('.grid');
+// var _iso = new Isotope( '.grid', {
+//   // options
+//   itemSelector: '.grid-item',
+//   layoutMode: 'fitRows',
+//   gutter: '20px'
+// });
 
-// element argument can be a selector string
-//   for an individual element
+// // element argument can be a selector string
+// //   for an individual element
 
-// var iso = new Isotope('.grid');
-// container.appendChild( elem );
-// iso.appended( elem );
-_iso.layout();
-
-
+// // var iso = new Isotope('.grid');
+// // container.appendChild( elem );
+// // iso.appended( elem );
+// _iso.layout();
 
 
 
 
-	// filter
-// country
-// ggc
-// expo tech
-// industry
-// company?
-// what you want to get out?
 
-	// sort - up down for all
-// alpha
-// industry
-// ?
 
-	// general data
-// avg age
-// country makeup
-// industry
-// ?
+// 	// filter
+// // country
+// // ggc
+// // expo tech
+// // industry
+// // company?
+// // what you want to get out?
+
+// 	// sort - up down for all
+// // alpha
+// // industry
+// // ?
+
+// 	// general data
+// // avg age
+// // country makeup
+// // industry
+// // ?
