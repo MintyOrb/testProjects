a54ca2c463bc25671cb391b0a1b763fbadef5cab
bornytm <bornytm@gmail.com>
2016-07-18 10:33:42 -0700

    addtional force, packing, and projection tests

--- d3/test.js
+++
@@ -1,9 +0,0 @@
-var theData = [ 1, 2, 3 ]
-
-var p = d3.select("body").selectAll("p")
-  .data(theData)
-  .enter()
-  .append("p")
-  .text(function (d,i) {
-    return "i = " + i + " d = "+d;
-   });
